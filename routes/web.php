36b0bb3b2ebbead5bf3fc7b9303f3206846268b2
AdiTechDeveloper <aditechdeveloper@gmail.com>
2026-08-12 10:51:18 +0530

    remove some code in web.php

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // ADMIN PANEL
 Route::prefix('admin')
     ->name('admin.')
