bbd58b1e4a593c1133c8d4644446705b1c0a6c1d
AdiTechDeveloper <aditechdeveloper@gmail.com>
2026-08-12 17:16:35 +0530

    minor changes in sidebar and poojacontroller

--- routes/api.php
+++ routes/api.php
@@ -62,3 +62,4 @@ Route::middleware('auth:sanctum')->group(function () {
     );
     Route::get('/admin/memberships', [MembershipController::class, 'index']);
 });
+Route::get('/poojas', [PoojaController::class, 'publicIndex']);
